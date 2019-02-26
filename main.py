e436b288e7e2e17746ed8a6b378461a6cc7e6b6d
alex <rai>
2019-02-26 17:24:05 +0700

    some more information

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #import depencies
-from flask import Flask
+from flask import Flask, render_template
 
 #Create instance of flask app
 app = Flask(__name__)
@@ -7,12 +7,12 @@ app = Flask(__name__)
 #Define route to index and content
 @app.route('/')
 def home():
-    return ('Home Page')
+    return render_template('Home.html')
 
 #create page with information about us
-@app.route('/about')
+@app.route('/blog')
 def about():
-    return('Some information about me')
+    return render_template('blog.html')
 
 
 #run and control script
