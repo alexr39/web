748b79132063e23102a9e4961f8abb1a375a4a0a
alex <rai>
2019-02-26 16:05:06 +0700

    some more information

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ from flask import Flask
 #Create instance of flask app
 app = Flask(__name__)
 
-#Define route to index
+#Define route to index and content
 @app.route('/')
-
-#content
 def home():
     return ('Home Page')
+
+#create page with information about us
+@app.route('/about')
+def about():
+    return('Some information about me')
 
 
 #run and control script
